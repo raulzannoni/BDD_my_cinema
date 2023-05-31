2b4ed14faf834bf948391d5536d4aa0eee7a8e11
raul.zannoni <raul.zannoni92@gmail.com>
2023-05-31 14:17:33 +0200

    Request B

--- cinema_request.sql
+++ cinema_request.sql
@@ -7,8 +7,9 @@ SELECT f.title_film AS "Movie title",
 FROM film f, person p, director d
 WHERE f.id_director = (SELECT d.id_director WHERE d.id_person = p.id_person) AND	f.id_film = 1
 
-/*-- b. Liste des films dont la durée excède 2h15 classés par durée (du plus long au plus court) --*/
-SELECT f.title_film AS "Movies longer than 2h15", MINUTE(f.duration_film) AS "Duration (min)"
-FROM FILM as film
-WHERE MINUTE(f.duration_film) > 135
-ORDER BY MINUTE(f.duration_film) DESC
+/*-- B. Liste des films dont la durée excède 2h15 classés par durée (du plus long au plus court) --*/
+SELECT f.title_film AS "Movies longer than 2h15", 
+	CEIL(TIME_TO_SEC(f.duration_film)/60) AS "Duration (min)"
+FROM film f
+WHERE CEIL(TIME_TO_SEC(f.duration_film)/60) > 135
+ORDER BY CEIL(TIME_TO_SEC(f.duration_film)/60) DESC

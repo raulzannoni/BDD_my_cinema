34132dbb5b7269dcaea214d4f975e2ef973028e7
raul_zannoni <raul.zannnoni92@gmail.com>
2023-06-01 10:30:09 +0200

    Request A correction

--- cinema_request.sql
+++ cinema_request.sql
@@ -5,7 +5,8 @@ SELECT f.title_film AS "Movie title",
 	p.first_name_person AS "Director", 
 	p.name_person AS ""
 FROM film f, person p, director d
-WHERE f.id_director = (SELECT d.id_director WHERE d.id_person = p.id_person) AND f.id_film = 1
+WHERE f.id_director = (SELECT d.id_director WHERE d.id_person = p.id_person) 
+	AND f.id_film = (SELECT f.id_film WHERE f.title_film = "Alien")
 
 /*-- B. Liste des films dont la durée excède 2h15 classés par durée (du plus long au plus court) --*/
 SELECT f.title_film AS "Movies longer than 2h15", 

fa8257f76d23d7d5a50909154e47041c57981162
raul.zannoni <raul.zannoni92@gmail.com>
2023-06-14 14:20:57 +0200

    edit and delete Genre complete

--- controller/CinemaController.php
+++ controller/CinemaController.php
@@ -337,8 +337,8 @@ class CinemaController
                                     AND t.id_film = f.id_film
                                     GROUP BY tp.id_type_film, tp.name_type_film
                                     ORDER BY COUNT(f.title_film) DESC";
-                $sql_text = "SELECT * from type_film";
-                $db_genreList = $pdo->query($sql_text);
+                //$sql_text = "SELECT * from type_film";
+                $db_genreList = $pdo->query($sql_genreList);
                 require "view/genres/genreList.php";
             }
         public function genreDetail($id)
@@ -462,15 +462,135 @@ class CinemaController
                     }
                 require "view/genres/addGenre.php";
             }
-        public function editGenre()
+        public function editGenre($id)
             {
                 $pdo = Connect::dbConnect();
+                $sql_genreList = "SELECT * FROM type_film tp";
+                $db_genreList = $pdo->query($sql_genreList);
+
+                $sql_genreDetail = "SELECT * FROM type_film tp
+                                    WHERE tp.id_type_film = :id";
+                $db_genreDetail = $pdo->prepare($sql_genreDetail);
+                $db_genreDetail->execute(["id" => $id]);
+
+                if(isset($_POST['submit']))
+                    {
+                        $genre = filter_input(INPUT_POST, "genre", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+                        $description = filter_input(INPUT_POST, "description", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+                        
+                        if(isset($_FILES['portrait']))
+                            {
+                                $imgTmpName = $_FILES['portrait']['tmp_name'];
+                                $imgName = $_FILES['portrait']['name'];
+                                $imgSize = $_FILES['portrait']['size'];
+                                $imgError = $_FILES['portrait']['error'];
+                                
+                                //on va a prendre le dernier element de le nome de le file (l'extension)
+                                $tabExtension = explode('.', $imgName);
+                                $extension = strtolower(end($tabExtension));
+
+                                //on va a verifier que l'extension de le file ajouté soit effectivement d'une image
+                                $extensions = ['jpg', 'png', 'jpeg', 'gif'];
+                                $maxSize = 500000000;
+                                
+                                //pour envoyer l'image dans notre dossier, on doit verifier que l'estension soit correct, 
+                                //qu'il n'y ait pas des errors et que le dimension de l'image soit raisonnable
+                                if(in_array($extension, $extensions) && $imgSize <= $maxSize && $imgError == 0)
+                                    {
+                                        //uniqid va à créer un ID unique et aleatoire
+                                        $uniqueName = uniqid('', true);
+                                        
+                                        //on va à créer la variable img = ID + extension
+                                        $portrait = $uniqueName.'.'.$extension;
+
+                                        //On doit exprimer le chemin de le dossier où les images doivent etré stockées (sur le Mac)
+                                        $path = "/Applications/XAMPP/xamppfiles/htdocs/raul_ZANNONI/BDD_my_cinema/public/img/person";
+
+                                        //fonction poutr envoyer l'image dans le dossier upload
+                                        move_uploaded_file($imgTmpName, './public/img/person/'.$portrait);
+                                        
+                                        //si le chargement n'est pas marché, on utilise le PATH complet de le dossier "upload"
+                                        if(!move_uploaded_file($imgTmpName, './public/img/person/'.$portrait))
+                                            {
+                                                move_uploaded_file($imgTmpName, $path.$portrait);
+                                            }
+                                    }
+                                else
+                                    {
+                                        $portrait = NULL; 
+                                        $_SESSION['message'] = "<p class='insuccess fadeOut'>Mauvaise extension ou image trop volumineuse!</p>";
+                                    }
+                            }
+                        else
+                            {
+                                $portrait = NULL; 
+                            }
+                        
+                        $genreExist = TRUE;
+
+                        foreach($db_genreList->fetchAll() as $genres)
+                            {
+                                if($genres['id_type_film'] == $id)
+                                    {
+                                        continue;
+                                    }
+                                else
+                                    {
+                                        if(strtolower($genres['name_type_film']) == strtolower($genre))
+                                            {
+                                                $genreExist = TRUE;
+                                                break;
+                                            }
+                                        else
+                                            {
+                                                $genreExist = FALSE;
+                                            }
+                                    }
+                            }
+                        
+                        if($genreExist)
+                            {
+                                $_SESSION['message'] = "<p class='insuccess fadeOut'>Le genre de film ajouté exist déjà...</p>";
+                                header("Location:index.php?action=editGenre");
+                            }
+                        else
+                            {  
+                                if(!$description)
+                                    {
+                                        $description = NULL;
+                                    }
+                        
+                                $sql_editGenre =    "UPDATE type_film
+                                                    SET name_type_film = :name_type_film, 
+                                                        poster_type_film = :poster_type_film, 
+                                                        description_type_film = :description_type_film
+                                                    WHERE id_type_film = :id";
+                                $db_editGenre = $pdo->prepare($sql_editGenre);
+
+                                $db_editGenre->bindValue(":name_type_film", $genre);
+                                $db_editGenre->bindValue(":poster_type_film", $portrait);
+                                $db_editGenre->bindValue(":description_type_film", $description);
+                                $db_editGenre->bindValue(":id", $id);
+                                
+                                $db_editGenre->execute();
+                                header("Location:index.php?action=genreList");
+                            }
+                    }
+
+
                 require "view/genres/editGenre.php";
             }
-        public function deleteGenre()
+        public function deleteGenre($id)
             {
                 $pdo = Connect::dbConnect();
-                require "view/genres/deleteGenre.php";
+                $sql_deleteGenre = "DELETE FROM type_film
+                                    WHERE id_type_film = :id";
+                $db_deleteGenre = $pdo->prepare($sql_deleteGenre);
+                $db_deleteGenre->bindParam(":id", $id);
+
+                $db_deleteGenre->execute();
+
+                header("Location:index.php?action=genreList");
             }
         public function checkGenre($id)
             {
@@ -478,7 +598,9 @@ class CinemaController
                 $sql_checkGenre =  "SELECT * FROM type_film
                                     WHERE id_type_film = :id";
                 $db_checkGenre = $pdo->prepare($sql_checkGenre);
-                $db_checkGenre->execute(["id" => $id]);
+                $db_checkGenre->bindParam(":id", $id);
+
+                $db_checkGenre->execute();
                 $genreExist = $db_checkGenre->fetch();
                 
                 if (!empty($genreExist)) {$result = TRUE;}
---
+++ view/genres/editGenre.php
@@ -0,0 +1,48 @@
+<?php 
+
+ob_start(); 
+
+if(isset($_SESSION['message'])) 
+    {
+        echo $_SESSION['message'];
+        unset($_SESSION['message']);
+    }
+
+$genreDetail = $db_genreDetail->fetch();
+
+?>
+
+<div id="Edit Genre">
+    <h2>Cinema_BDD</h2>
+    <p>Edit Genre</p>
+</div>
+
+<div class="p-2">
+    <form class="row w-50 g-3 p-6 m-3 border" action="index.php?action=editGenre&id=<?= $genreDetail['id_type_film']?>" method="post" enctype="multipart/form-data" autocomplete="off">
+        <div class="col-md-6">
+            <label for="genre" class="form-label">Genre :
+                <input type="text" class="form-control" name="genre" id="genre" value="<?= $genreDetail['name_type_film']; ?>">
+            </label>
+        </div>
+        <div class="col-md-6">
+            <label for="description" class="form-label">Description :
+                <input type="text" class="form-control" name="description" id="description" value="<?= $genreDetail['description_type_film']; ?>">
+            </label>
+        </div>
+        <div class="col-md-6">
+            <label for="poster" class="form-label">Poster :
+                <input type="file" class="form-control" name="poster" id="poster" value="<?= $genreDetail['poster_type_film']; ?>">
+            </label>
+        </div>
+        <div class="col-12">
+            <button type="submit" name="submit" class="btn btn-primary">Update : </button>
+        </div>
+    </form>
+</div>
+
+
+<?php
+$title = "Edit Genre";
+$content = ob_get_clean();
+require "view/template.php";
+?>
